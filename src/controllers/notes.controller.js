12d4197f4e69eba201e0db6f668c96ae49c9ecc9
LakshAgrawal28 <sahilvinod544@gmail.com>
2025-07-01 20:41:45 +0530

    notes controller added

---
+++ src/controllers/notes.controller.js
@@ -0,0 +1,13 @@
+import { Note } from '../models/note.model.js';
+
+export const getNotes = async (req, res) => {
+  const notes = await Note.find({ user: req.user._id });
+  res.json(notes);
+};
+
+export const createNote = async (req, res) => {
+  const { title, content } = req.body;
+  const note = await Note.create({ title, content, user: req.user._id });
+  res.status(201).json(note);
+};
+
